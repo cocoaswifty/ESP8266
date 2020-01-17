980cb67d1520fea0274c6896a2f28c57eb0d3cc8
cocoaswifty <cocoaswifty@gmail.com>
2020-01-17 13:28:18 +0800

    fix mqtt publisher

--- Watering/main.py
+++ Watering/main.py
@@ -24,18 +24,25 @@ from servo import Servo
 import urequests as req
 import ubinascii
 import machine
+import network
+import ntptime
 import math
 import time
 import dht
-import network
-import ntptime
+
+
+def init():
+    rtc = machine.RTC()
+    if rtc.memory() == b'':
+        setUTC8Time()  # 校正時間
+        rtc.memory(b'0')  # 存入0來代表已校正時間
 
 
 def connectAP():
     wlan = network.WLAN(network.STA_IF)  # 設定成STA模式
     if not wlan.isconnected():
         wlan.active(True)  # 啟用無線網路
-        wlan.connect('City free wifi', 'XXX')
+        wlan.connect('XXX', 'XXX')
 
     timeout = time.time() + 10   # 連線超過 10 秒 跳出
     while not wlan.isconnected():  # 等待，直到連線成功
@@ -47,21 +54,20 @@ def connectAP():
     return wlan
 
 
-def setUTC8Time(rtc):  # 校正時間
-    if rtc.memory() == b'':
-        wlan = connectAP()  # 連網
-        try:
-            t = ntptime.time() + 28800  # 加8小時
-            tm = time.localtime(t)
-            machine.RTC().datetime(
-                tm[0:3] + (0, ) + tm[3:6] + (0, ))  # 轉成RTC格式
-            rtc.memory(b'0')  # 存入0來代表已校正時間
-        except Exception as e:
-            print('Error!', e)
+def setUTC8Time():  # 校正時間
+    wlan = connectAP()  # 連網
+    try:
+        t = ntptime.time() + 28800  # 加8小時
+        tm = time.localtime(t)
+        machine.RTC().datetime(
+            tm[0:3] + (0, ) + tm[3:6] + (0, ))  # 轉成RTC格式
 
-        wlan.disconnect  # 斷網
-    # print('now:', time.localtime())
+        # print('now:', time.localtime())
+    except Exception as e:
+        print('Error!', e)
 
+    wlan.disconnect  # 斷網
+    
 
 def get_dew_point_c(t_air_c, rel_humidity):  # 計算露點溫度
     A = 17.27
@@ -93,7 +99,7 @@ def publishMqtt(data):
         time.sleep(2)
         client.disconnect()
     except Exception as e:
-        # print('could not connect to MQTT server {}{}'.format(type(e).__name__, e))
+        print('could not connect to MQTT server {}{}'.format(type(e).__name__, e))
         client.disconnect()
 
     wlan.disconnect
@@ -113,9 +119,10 @@ def publishIFTTT(watering_time):
 
 
 def watering(soil, dew):
-    if soil >= 700 and dew <= 22:   # 水分高於 700, 露點溫度小於22
+    if soil <= 500 and dew <= 22:   # 水分低於 700, 露點溫度小於22
         conds = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]  # 白天才澆水
         tm_hour = time.localtime()[3]  # 時
+
         if tm_hour in conds:
             watering_time = 0   # 持續澆水時間
             publishIFTTT(watering_time)
@@ -124,58 +131,58 @@ def watering(soil, dew):
             sg90 = Servo(13)  # D7 伺服馬達
             sg90.rotate(180)    # 開水
 
-            while watering_time <= 5*60:    # 如果澆水超過5分鐘
+            while watering_time < 5*60:    # 如果澆水超過5分鐘
                 time.sleep(10)
                 watering_time += 10
                 if soil >= 900:
                     sg90.rotate(0)      # 關水
                     break
+
             sg90.rotate(0)      # 關水
 
             time.sleep(5)
+            publishIFTTT(watering_time)
             data = 'field6={}'.format(watering_time)
             publishMqtt(data)
-            publishIFTTT(watering_time)
+            
 
 
-if __name__ == '__main__':
-    rtc = machine.RTC()  # GPIO16(D0),輸出低電位訊號，接一個1KΩ到RST腳位
-    setUTC8Time(rtc)  # 校正時間
-    # 觸發來源ALARM0, deep sleep模式
-    rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
 
-    tm_min = time.localtime()[4]
-    if tm_min != 0:
-        rtc.alarm(rtc.ALARM0, (60 - tm_min)*60*1000)    # 等待X秒到整點喚醒
-    elif tm_min == 0:
-        rtc.alarm(rtc.ALARM0, 60*60*1000)  # 1小時喚醒一次
+init()
+rtc = machine.RTC()  # GPIO16(D0),輸出低電位訊號，接一個1KΩ到RST腳位
+# 觸發來源ALARM0, deep sleep模式
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
 
-    if machine.reset_cause() == machine.DEEPSLEEP_RESET and tm_min == 0:    # 導致重置因素
-        dht11 = Pin(14, Pin.IN)  # D5 溫濕度傳感器
-        d = dht.DHT11(dht11)
-        d.measure()  # 開始測量
+tm_min = time.localtime()[4]
+tm_sec = time.localtime()[5]
+# print('time.localtime():', time.localtime())
 
-        temp = d.temperature()  # 溫度
-        humi = d.humidity()   # 濕度
-        dew = get_dew_point_c(temp, humi)  # 露點溫度
-        adc = ADC(0)  # 類比輸入0腳位
-        soil = 1024 - adc.read()  # 讀取 土壤濕度類比值 1024=乾旱, 越小越濕
-        yl69 = Pin(12, Pin.IN)  # D6 土壤濕度感測器
-        dry = yl69.value()    # 土壤乾旱 0=潮濕 1=乾旱
+rtc.alarm(rtc.ALARM0, ((60-tm_min)*60-tm_sec)*1000)    # 等待X秒到整點喚醒
 
-        # print('溫度：', str(temp))
-        # print('濕度：', str(humi))
-        # print('露點溫度：', str(dew))
-        # print('土壤濕度類比值：', str(soil))
-        # print('土壤乾旱 0=潮濕 1=乾旱：', str(dry))
-        # print('現在時間：', time.localtime())
-        # print('時：', time.localtime()[3])
-        # print('分：', time.localtime()[4])
 
-        data = 'field1={}&field2={}&field3={}&field4={}&field5={}'.format(
-            temp, humi, dew, soil, dry)
-        publishMqtt(data)
-        watering(soil, dew)
+if machine.reset_cause() == machine.DEEPSLEEP_RESET:    # 導致重置因素
+    dht11 = Pin(14, Pin.IN)  # D5 溫濕度傳感器
+    d = dht.DHT11(dht11)
+    d.measure()  # 開始測量
 
-    # print('Going to sleep...')
-    machine.deepsleep()
+    temp = d.temperature()  # 溫度
+    humi = d.humidity()   # 濕度
+    dew = get_dew_point_c(temp, humi)  # 露點溫度
+    adc = ADC(0)  # 類比輸入0腳位
+    soil = 1024 - adc.read()  # 讀取 土壤濕度類比值 1024=乾旱, 越小越濕
+    yl69 = Pin(12, Pin.IN)  # D6 土壤濕度感測器
+    dry = yl69.value()    # 土壤乾旱 0=潮濕 1=乾旱
+
+    # print('溫度：', str(temp))
+    # print('濕度：', str(humi))
+    # print('露點溫度：', str(dew))
+    # print('土壤濕度類比值：', str(soil))
+    # print('土壤乾旱 0=潮濕 1=乾旱：', str(dry))
+
+    data = 'field1={}&field2={}&field3={}&field4={}&field5={}'.format(
+        temp, humi, dew, soil, dry)
+    publishMqtt(data)
+    watering(soil, dew)
+
+# print('Going to sleep...')
+machine.deepsleep()
